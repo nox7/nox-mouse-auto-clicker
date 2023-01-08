58a692cc771dca3474474cb8f4d0b729a8edcd9f
Cole Green <17110935+nox7@users.noreply.github.com>
2023-01-08 10:25:23 -0600

    Create GlobalKeyboardHookUtility.cs

---
+++ Interval Auto Clicker/KeyboardUtility/GlobalKeyboardHookUtility.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interval_Auto_Clicker.KeyboardUtility
+{
+    internal class GlobalKeyboardHookUtility
+    {
+
+        private MainWindow mainWindowInstance;
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private static LowLevelKeyboardProc? _proc = null;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        // Start hotkey
+        private const uint VK_F11 = 0x7A;
+
+        public GlobalKeyboardHookUtility(MainWindow mainWindow)
+        {
+            mainWindowInstance = mainWindow;
+            Debug.WriteLine("Init global keyboard hook utility.");
+            _proc = HookCallback;
+            _hookID = SetHook(_proc);
+        }
+
+        private IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                Debug.WriteLine("Hook set for current process and module.");
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private IntPtr HookCallback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int virtualKeyKeyCode = Marshal.ReadInt32(lParam);
+                Debug.WriteLine("Pressed virtual key: " + virtualKeyKeyCode);
+                mainWindowInstance.onStartKeyPressed();
+            }
+
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr SetWindowsHookEx(int idHook, LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll")]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        [DllImport("user32.dll")]
+        private static extern short GetAsyncKeyState(uint vKey);
+    }
+}
