5830f210d9f043e405b0c79035b05afe571bf7c7
Cole Green <17110935+nox7@users.noreply.github.com>
2023-01-08 10:25:34 -0600

    Integrate global hook listener + start flag

--- Interval Auto Clicker/MainWindow.xaml.cs
+++ Interval Auto Clicker/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Interval_Auto_Clicker.KeyboardUtility;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,10 +23,26 @@ namespace Interval_Auto_Clicker
     /// </summary>
     public partial class MainWindow : Window
     {
+        public bool isRunning = false;
+
         public MainWindow()
         {
+            new GlobalKeyboardHookUtility(this);
             InitializeComponent();
         }
 
+        public void onStartKeyPressed()
+        {
+            if (!isRunning)
+            {
+                isRunning = true;
+                Debug.WriteLine("Auto clicker is now running.");
+            }
+            else
+            {
+                // Ignore, it's already running.
+                Debug.WriteLine("Cannot start. Already running.");
+            }
+        }
     }
 }
